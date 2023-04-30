b57fec52280ec1a45431b7290de8281e13f6595e
vinliang888 <vin.liang888@gmail.com>
2023-04-29 23:29:30 -0700

    completed 19.2 exercises

---
+++ 19.2-flask-madlibs/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import Story
+
+app = Flask(__name__)
+
+STORY = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
+
+@app.route('/')
+def generate_story_prompts():
+    return render_template("story-generator.html", prompts = STORY.prompts)
+
+@app.route('/story')
+def create_story():
+    story_text = STORY.generate(request.args.to_dict())
+    return render_template("story-display.html", story=story_text)
